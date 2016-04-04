92f31fb54f8b05a51519a6e8d962188116fd2742
Karin Touchie <ktouchie@nuxeo.com>
2016-04-04 18:15:39 +0200

    CHROME-8: Add auto search functionality using input GUID or path and displaying JSON in new popup window

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -35,6 +35,29 @@ getCurrentTabUrl(function(url) {
 });
 
 $(document).ready(function() {
+
+  function openJsonWindow(jsonObject) {
+    var jsonString;
+    var w = 600;
+    var h = 800;
+    var left = (screen.width/2)-(w/2);
+    var top = (screen.height/2)-(h/2);
+    jsonString = JSON.stringify(jsonObject, undefined, 2);
+    jsonString = btoa(jsonString);
+    chrome.windows.create({'url': 'data:application/json;base64,' + jsonString, 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {});
+  };
+
+  function debounce(fn, delay) {
+    var timer = null;
+    return function () {
+      var context = this, args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function () {
+        fn.apply(context, args);
+      }, delay);
+    };
+  }
+
   $('#hot-reload-button').click(function() {
     var bkg = chrome.extension.getBackgroundPage();
     nuxeo.operation('Document.HotReloadOperation').execute()
@@ -52,11 +75,13 @@ $(document).ready(function() {
         };
       });
   });
+
   $('#autodoc-button').click(function() {
     chrome.tabs.create({
       url: nuxeo._baseURL.concat('site/automation/doc/')
     });
   });
+
   $('#debug-switch').click(function() {
     nuxeo.operation('Traces.ToggleRecording')
       .params({readOnly: false})
@@ -64,5 +89,32 @@ $(document).ready(function() {
       .then(function(response) {
         $('#debug-switch').attr('checked', response.value);
       })
-    });
+  });
+
+  $('#json-search').keyup(debounce(function() {
+    var uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    var pathPattern = new RegExp('^/.+');
+    var input = $('#json-search').val();
+    if (uuidPattern.test(input)) {
+      nuxeo.request('/id/' + input)
+        .schemas('*')
+        .get()
+        .then(function(res) {
+          openJsonWindow(res);
+        })
+        .catch(function(error) {
+          throw new Error(error);
+        });
+    } else if (pathPattern.test(input)) {
+      nuxeo.request('/path/' + input)
+        .schemas('*')
+        .get()
+        .then(function(res) {
+          openJsonWindow(res);
+        })
+        .catch(function(error) {
+          throw new Error(error);
+        });
+    }
+  }, 2000));
 });
