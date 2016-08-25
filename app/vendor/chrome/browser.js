9eaedc7bea2efc57065b91e0a6f0c637b6cef397
Karin Touchie <ktouchie@nuxeo.com>
2016-08-25 13:26:40 +0200

    CHROME-31: Fix browser module and manifest

--- app/vendor/chrome/browser.js
+++ app/vendor/chrome/browser.js
@@ -7,15 +7,14 @@
 
 		getBackgroundPage: function() {
 			return chrome.runtime.getBackgroundPage;
-		}
+		},
 
 		getUrl: function (url) {
 			return chrome.extension.getURL(url);
-		}
+		},
 
 		createTabs: function() {
 			return chrome.tabs.create;
 		}
 	};
 })(window);
-console.log("YOYOYO");
