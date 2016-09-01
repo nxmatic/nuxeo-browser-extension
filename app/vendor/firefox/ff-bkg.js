630b4e1e8860cab007d09dd19cf09b579e119320
Karin Touchie <ktouchie@nuxeo.com>
2016-09-01 16:47:39 +0200

    CHROME-31: Workaround so FF addon can function as browser action

--- app/vendor/firefox/ff-bkg.js
+++ app/vendor/firefox/ff-bkg.js
@@ -1,9 +1,34 @@
+chrome.browserAction.disable();
+disableIcon();
+
+function disableIcon(tabId) {
+  chrome.browserAction.setIcon({ path: {
+    '16': '../images/nuxeo-grey-16.png',
+    '19': '../images/nuxeo-grey-19.png',
+    '32': '../images/nuxeo-grey-32.png',
+    '38': '../images/nuxeo-grey-38.png'
+  }, tabId: tabId });
+}
+
+function enableIcon(tabId) {
+  chrome.browserAction.setIcon({ path: {
+    '16': '../images/nuxeo-16.png',
+    '19': '../images/nuxeo-19.png',
+    '32': '../images/nuxeo-32.png',
+    '38': '../images/nuxeo-38.png'
+  }, tabId: tabId });
+}
+
 function pageActionOnNuxeo(tabInfo) {
-  var re = /https?:\/\/[\w\:\.]+\/\w+\/(?:nxdoc|nxpath|nxsearch|nxadmin|nxhome|nxdam|nxdamid|site\/\w+)\/\w+/g;
+  var re = /https?:\/\/[\w\:\.]+\/\w+\/(?:(?:nxdoc|nxpath|nxsearch|nxadmin|nxhome|nxdam|nxdamid|site\/\w+)\/\w+|view_documents\.faces|view_domains\.faces|view_home\.faces)/g;
   var currentUrl = tabInfo.url;
   var isNuxeo = currentUrl.match(re);
-  if (isNuxeo){
-    chrome.pageAction.show(tabInfo.id);
+  if (isNuxeo === null){
+    disableIcon();
+    chrome.browserAction.disable(tabInfo.id);
+  } else {
+    enableIcon();
+    chrome.browserAction.enable(tabInfo.id);
   };
 };
 
@@ -19,4 +44,5 @@ function onChange(tabInfo) {
 
 var target = "<all_urls>";
 chrome.webRequest.onCompleted.addListener(onChange, {urls: [target]});
+chrome.tabs.onActivated.addListener(onChange);
 
