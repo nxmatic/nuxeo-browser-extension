535f7a06baf3c9087e3cd4ccda12eab3e24b6f5b
Karin Touchie <ktouchie@nuxeo.com>
2017-07-28 18:30:10 +0200

    BDE-73: Add link to propdpad feedback portal

--- app/scripts.babel/about.js
+++ app/scripts.babel/about.js
@@ -12,6 +12,9 @@ app.browser.getBackgroundPage(function(bkg) {
   $('#apache').click(function() {
     app.browser.createTabs('http://www.apache.org/licenses/LICENSE-2.0', bkg.studioExt.server.tabId);
   });
+  $('#feedback').click(function() {
+    app.browser.createTabs('https://portal.prodpad.com/40c295d6-739d-11e7-9e52-06df22ffaf6f', bkg.studioExt.server.tabId);
+  });
 });
 
 $.ajax({
